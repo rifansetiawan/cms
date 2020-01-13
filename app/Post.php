93697a5489c1646b023b39ade905b8ea0ecd89c2
rifansetiawan <37042039+rifansetiawan@users.noreply.github.com>
2020-01-13 23:16:15 +0700

    Update Post.php

--- app/Post.php
+++ app/Post.php
@@ -3,15 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Post extends Model
 {
     //
     // $protected 
+    use SoftDeletes;
+    
     protected $fillable = [
 
         'title',
         'content'
 
     ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
